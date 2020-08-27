51d1aa311e15c55de749c6d71c2a7ff70112e7ba
wdy06 <mituhashitakashi@gmail.com>
2020-08-27 04:44:00 +0000

    udpate upload script

--- upload_dataset.sh
+++ upload_dataset.sh
@@ -2,7 +2,8 @@
 
 echo $1
 EXP_NAME=$1
-kaggle datasets init -p results/$EXP_NAME
-sed -i -e s/INSERT_TITLE_HERE/$EXP_NAME/ results/$EXP_NAME/dataset-metadata.json
-sed -i -e s/INSERT_SLUG_HERE/$EXP_NAME/ results/$EXP_NAME/dataset-metadata.json
-kaggle datasets create -p results/$EXP_NAME
+kaggle datasets init -p outputs/$EXP_NAME
+sed -i -e s/INSERT_TITLE_HERE/$EXP_NAME/ outputs/$EXP_NAME/dataset-metadata.json
+sed -i -e s/INSERT_SLUG_HERE/$EXP_NAME/ outputs/$EXP_NAME/dataset-metadata.json
+kaggle datasets create -p outputs/$EXP_NAME -r zip
+
